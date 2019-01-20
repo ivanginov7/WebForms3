3cf75fe35301a8b8816acfbf5ad4ffcac31673b4
ivanginov7 <ivanginov7@gmail.com>
2019-01-20 22:28:19 +0200

    Revert "AdminDashboard GetProducts"
    This reverts commit 2fd240f49f16cfb0f759fbab3bc93e747b2d8557.

--- WebForms3/Admin/AdminDashboard.aspx
+++ WebForms3/Admin/AdminDashboard.aspx
@@ -1,23 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdminDashboard.aspx.cs" Inherits="WebForms3.Admin.AdminDashboard" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <asp:GridView runat="server" ID="productsGrid"
-        ItemType="WebForms3.Models.ViewModels.ProductsWithCategoriesViewModel" DataKeyName="ProductID"
+        ItemType="WebForms3.Models.Product" DataKeyName="ProductID"
         SelectMethod="productsGrid_GetData"
         AutoGenerateColumns="false">
         <Columns>
             <asp:DynamicField DataField="ProductID" />
             <asp:DynamicField DataField="ProductName" />
             <asp:DynamicField DataField="Description" />
-            <asp:ImageField DataImageUrlField="ImagePath" />
+            <asp:DynamicField DataField="ImagePath" />
             <asp:DynamicField DataField="UnitPrice" />
-            <asp:HyperLinkField 
-                HeaderText="Edit"
-                DataTextField="ProductID"
-                DataNavigateUrlFields="ProductID"
-                DataNavigateUrlFormatString="~/Admin/EditProduct.aspx?ProductID={0}" />
+            
         </Columns>
     </asp:GridView>
-    
-    <asp:Label Text="<%#ImagePath%>" runat="server" />
-    
 </asp:Content>
--- WebForms3/Admin/AdminDashboard.aspx.cs
+++ WebForms3/Admin/AdminDashboard.aspx.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebForms3.Models;
-using WebForms3.Models.ViewModels;
 
 namespace WebForms3.Admin
 {
@@ -13,43 +12,20 @@ namespace WebForms3.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.ImagePath = this.getImagePath();
-            Page.DataBind();
+
         }
-        public string ImagePath; 
+
         // The return type can be changed to IEnumerable, however to support
         // paging and sorting, the following parameters must be added:
         //     int maximumRows
         //     int startRowIndex
         //     out int totalRowCount
         //     string sortByExpression
-        public IQueryable<ProductsWithCategoriesViewModel> productsGrid_GetData()
+        public IQueryable<WebForms3.Models.Product> productsGrid_GetData()
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            
-            IQueryable<ProductsWithCategoriesViewModel> query =
-                from p in db.Products
-                join c in db.Categories
-                on p.CategoryID equals c.CategoryID
-                select new ProductsWithCategoriesViewModel
-                {
-                    ProductID = p.ProductID,
-                    Description = p.Description,
-                    ImagePath = "~/Catalog/Images/"+p.ImagePath,
-                    ProductName = p.ProductName,
-                    UnitPrice=p.UnitPrice
-
-                };
+            IQueryable<Product> query = db.Products;
             return query;
-        }
-        public string getImagePath()
-        {
-            var data = productsGrid_GetData();
-            if (data != null)
-            {
-                var item = data.SingleOrDefault(p=>p.ProductID==1);
-                return item.ImagePath;
-            } else { return "Oshit"; }
         }
     }
 }
